90a1c1b2921685aa307b5124d1425f04e01009b1
Akhmad Khudri <khudri@binadarma.ac.id>
2025-09-17 10:56:47 +0700

    Add STC GasX link to the app list

--- app.py
+++ app.py
@@ -63,6 +63,7 @@ with st.sidebar:
     8. [Learn3](https://learn3.streamlit.app/)
     9. [Nexus](https://rantai-nexus.streamlit.app/)
     10. [BlockPedia](https://blockpedia.streamlit.app/)
+    11. [STC GasX](https://stc-gasx.streamlit.app/)
 
     ---
     #### 🙌 Dukungan & kontributor
